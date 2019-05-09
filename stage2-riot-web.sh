51dcb0cd4b0bb7cbe6bdde594e977a33d79f5878
Alex Morega <alex@grep.ro>
2019-05-10 01:26:45 +0300

    Clean up teh scriptures

--- stage2-riot-web.sh
+++ stage2-riot-web.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -ex
 
 git clone https://github.com/vector-im/riot-web
-( cd riot-web; git checkout v0.13.5 )
+cd riot-web
+git checkout v0.13.5
+
+npm install
 
 (
-  cd riot-web
-  npm install
-  (
-    cd node_modules
-    rm -rf matrix-react-sdk
-    ln -s ../../matrix-react-sdk
-  )
-  npm run dist
+  cd node_modules
+  rm -rf matrix-react-sdk
+  ln -s ../../matrix-react-sdk
 )
+
+npm run dist
 
 mv riot-web/dist/riot-v0.13.5-dirty.tar.gz /tmp/riot.tgz
